de025dc0e4cf7cbb9260ad21a69c484fc3bce50a
egorbekesov <57090439+egorbekesov@users.noreply.github.com>
2019-12-09 12:23:50 +0300

    Update Exoplanet.py

--- Exoplanet.py
+++ Exoplanet.py
@@ -1,13 +1,29 @@
-import requests
-from urllib.request import urlopen
+import numpy as np
 from bs4 import BeautifulSoup
+import requests as req
 
-url = "http://exoplanet.eu/catalog/tres-3_b/" 
+name = input("Enter name of planet.")
 
-page = urlopen(url)
+first_date = float(input("Enter start date of observations."))
 
-soup=BeautifulSoup(page.read())
+end_date = float(input("Enter final date of observations."))
 
-value = soup.findAll(attrs={"class":"rowspan"})
+resp = req.get("http://exoplanet.eu/catalog/" + name + "/")
+ 
+soup = BeautifulSoup(resp.text, 'lxml')
 
-print(value[1])
+period = soup.find(id="planet_period_0").text
+
+period = float(period.split(" ")[0])
+
+last_transit = soup.find(id="planet_tzero_tr_0").text
+
+last_transit = float(last_transit.split(" ")[0])
+
+n0 = int((first_date - last_transit)/period)
+
+n1 = int((end_date - last_transit)/period)
+
+for n in range(n0+1, n1):
+    transit_date = last_transit + n*period
+    print(transit_date)
