1febff4e782218b84c9d3bb42243e83c98bfe7a4
egorbekesov <57090439+egorbekesov@users.noreply.github.com>
2019-12-23 12:31:07 +0300

    Add files via upload

--- Exoplanet.py
+++ Exoplanet.py
@@ -2,22 +2,21 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests as req
 
-def per(name):
+def pars(name):
     resp = req.get("http://exoplanet.eu/catalog/" + name + "/")
  
     soup = BeautifulSoup(resp.text, 'lxml')
-    
+
+    return soup
+
+def per(soup):
     period = soup.find(id="planet_period_0").text
 
     period = float(period.split(" ")[0])
     
     return period
 
-def ltr(name):
-    resp = req.get("http://exoplanet.eu/catalog/" + name + "/")
-    
-    soup = BeautifulSoup(resp.text, 'lxml')
-
+def ltr(soup):
     last_transit = soup.find(id="planet_tzero_tr_0").text
 
     last_transit = float(last_transit.split(" ")[0])
@@ -38,3 +37,18 @@ def Transit(period, last_transit, first_date, end_date):
         print(transit_date[n-n0])
     
     return transit_date
+
+name = input("Enter name of planet.")
+
+soup = pars(name)
+
+first_date = float(input("Enter the first day of observations"))
+
+end_date = float(input("Enter the last day of observations"))
+
+period = per(soup)
+
+last_transit = ltr(soup)
+
+Transit(period, last_transit, first_date, end_date)
+
