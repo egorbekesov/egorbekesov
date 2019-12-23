254875291851c17100c85e30093b073f3afa0829
egorbekesov <57090439+egorbekesov@users.noreply.github.com>
2019-12-23 15:05:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     version='1.0',
     test_suite='Test',
     py_modules = ["Exoplanet"],
-    install_requires=['reqests', 'bs4', 'lxml'],
+    install_requires=['requests', 'bs4', 'lxml'],
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
