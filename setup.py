012206d4232286a7ef685aaa8ed7416f92955c5e
egorbekesov <57090439+egorbekesov@users.noreply.github.com>
2019-12-23 15:01:19 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 from os.path import join, dirname
-from bs4 import BeautifulSoup
 
 setup(
     name='Exoplanet',
     version='1.0',
     test_suite='Test',
     py_modules = ["Exoplanet"],
+    install_requires=['reqests', 'bs4', 'lxml'],
     long_description=open(join(dirname(__file__), 'README.txt')).read(),
 )
